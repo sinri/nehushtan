d5ada3d85e18d5e4ee0069e834285088c33b4a7d
Sinri Edogawa <ljni@leqee.com>
2021-03-22 16:05:24 +0800

    Add Flag Support

--- nehushtan/cli/NehushtanArgumentParser.py
+++ nehushtan/cli/NehushtanArgumentParser.py
@@ -4,15 +4,22 @@ import sys
 
 class NehushtanArgumentParser:
     """
+    Since 0.2.19 Add Flag Support
     Support
-    <python xxx.py> [-a B] [--bb C] [--dd-ee F --dd-gg H]
-    Parsed as
+    <python xxx.py> [-a B] [--bb C] [--dd-ee F --dd-gg H] [-i] [--jj] [--kk-ll --kk-mm]
+    Suppose key is as argument name, the above would be parsed as
     {
         "a":"B"
         "bb":"C",
         "dd":{
             "ee":"F",
             "gg":"H",
+        },
+        "i":True,
+        "jj":True,
+        "kk":{
+            "ll":True,
+            "mm":True,
         }
     }
     """
@@ -27,9 +34,15 @@ class NehushtanArgumentParser:
 
         self._sys_arg_list = sys_arg_list
         self._argument_dict = {}
+
         self._options_short = {}
         self._options_long = {}
         self._options_prefix = {}
+
+        self._flags_short = {}
+        self._flags_long = {}
+        self._flags_prefix = {}
+
         self._result_dict = {}
         self._usage_dict = {}
 
@@ -40,7 +53,14 @@ class NehushtanArgumentParser:
         self._sys_arg_list = arg_list
         return self
 
-    def add_option(self, key: str, desc: str = '', short: str = '', long: str = '', prefix: str = ''):
+    def add_option(
+            self,
+            key: str, desc: str = '',
+            short: str = '',
+            long: str = '',
+            prefix: str = '',
+            is_flag=False
+    ):
         """
 
         :param desc:
@@ -48,6 +68,7 @@ class NehushtanArgumentParser:
         :param short: a -> `-a`
         :param long: alpha -> `--alpha`
         :param prefix: alef -> `--alef-`
+        :param is_flag: if its value follows existence only as boolean
         :return:
         """
         if short == '' and long == '' and prefix == '':
@@ -57,54 +78,103 @@ class NehushtanArgumentParser:
             'key': key,
             'desc': desc,
         }
+
         if short != '':
             argument_key = f'-{short}'
-            if self._options_short.get(argument_key) is not None:
-                raise RuntimeError(f'Short Key [{argument_key}] Already Existed')
-            self._options_short[argument_key] = key
-            usage['short'] = f'-{short} [VALUE]'
+            self.__assert_key_unique(argument_key, 'short')
+            if is_flag:
+                self._flags_short[argument_key] = key
+                usage['short'] = f'[-{short}]'
+            else:
+                self._options_short[argument_key] = key
+                usage['short'] = f'-{short} [VALUE]'
         if long != '':
             argument_key = f'--{long}'
-            if self._options_long.get(argument_key) is not None:
-                raise RuntimeError(f'Long Key [{argument_key}] Already Existed')
-            self._options_long[argument_key] = key
-            usage['long'] = f'--{long} [VALUE]'
+            self.__assert_key_unique(argument_key, 'long')
+            if is_flag:
+                self._flags_long[argument_key] = key
+                usage['long'] = f'[--{long}]'
+            else:
+                self._options_long[argument_key] = key
+                usage['long'] = f'--{long} [VALUE]'
         if prefix != '':
             argument_key = f'--{prefix}-'
-            if self._options_prefix.get(argument_key) is not None:
-                raise RuntimeError(f'Prefix Key [{argument_key}] Already Existed')
-            self._options_prefix[argument_key] = key
-            usage['prefix'] = f'--{prefix}-[KEY] [VALUE]'
+            self.__assert_key_unique(argument_key, 'prefix')
+            if is_flag:
+                self._flags_prefix[argument_key] = key
+                usage['prefix'] = f'[--{prefix}-[KEY]]'
+            else:
+                self._options_prefix[argument_key] = key
+                usage['prefix'] = f'--{prefix}-[KEY] [VALUE]'
 
         self._usage_dict[key] = usage
 
         return self
 
+    def __assert_key_unique(self, argument_key: str, key_type: str):
+        if key_type == 'short':
+            if self._flags_short.get(argument_key) is not None or self._options_short.get(argument_key) is not None:
+                raise RuntimeError(f'Short Key [{argument_key}] Already Existed')
+        elif key_type == 'long':
+            if self._flags_long.get(argument_key) is not None or self._options_long.get(argument_key) is not None:
+                raise RuntimeError(f'Long Key [{argument_key}] Already Existed')
+        elif key_type == 'prefix':
+            if self._flags_prefix.get(argument_key) is not None or self._options_prefix.get(argument_key) is not None:
+                raise RuntimeError(f'Prefix Key [{argument_key}] Already Existed')
+
     def parse(self):
+        self._result_dict = {}
+
         current_key = None
         self._argument_dict = {}
         for item in self._sys_arg_list:
             if current_key is None:
+                # check if current item is a flag key
+                if self.__parse_flag_key(item) is True:
+                    self._argument_dict[item] = True
+                    continue
+
                 current_key = item
             else:
                 self._argument_dict[current_key] = item
                 current_key = None
 
-        self._result_dict = {}
         for (key, value) in self._argument_dict.items():
-            if self._options_short.get(key) is not None:
-                self._result_dict[self._options_short[key]] = value
-            elif self._options_long.get(key) is not None:
-                self._result_dict[self._options_long[key]] = value
-            else:
-                for prefix, mapping_key in self._options_prefix.items():
-                    matches = re.match(f'^{prefix}([A-Za-z0-9._-]+)$', key)
-                    if matches is not None:
-                        if self._result_dict.get(mapping_key) is None:
-                            self._result_dict[mapping_key] = {}
-                        self._result_dict[mapping_key][matches[1]] = value
-                        break
+            self.__parse_option_pair(key, value)
+
         return self
+
+    def __parse_flag_key(self, key):
+        if self._flags_short.get(key) is not None:
+            self._result_dict[self._flags_short[key]] = True
+            return True
+        elif self._flags_long.get(key) is not None:
+            self._result_dict[self._flags_long[key]] = True
+            return True
+        else:
+            for prefix, mapping_key in self._flags_prefix.items():
+                matches = re.match(f'^{prefix}([A-Za-z0-9._-]+)$', key)
+                if matches is not None:
+                    if self._result_dict.get(mapping_key) is None:
+                        self._result_dict[mapping_key] = {}
+                    self._result_dict[mapping_key][matches[1]] = True
+                    return True
+        # not matched any
+        return False
+
+    def __parse_option_pair(self, key: str, value):
+        if self._options_short.get(key) is not None:
+            self._result_dict[self._options_short[key]] = value
+        elif self._options_long.get(key) is not None:
+            self._result_dict[self._options_long[key]] = value
+        else:
+            for prefix, mapping_key in self._options_prefix.items():
+                matches = re.match(f'^{prefix}([A-Za-z0-9._-]+)$', key)
+                if matches is not None:
+                    if self._result_dict.get(mapping_key) is None:
+                        self._result_dict[mapping_key] = {}
+                    self._result_dict[mapping_key][matches[1]] = value
+                    break
 
     def get_raw_option_dict(self):
         return self._argument_dict
@@ -130,7 +200,3 @@ class NehushtanArgumentParser:
             usage_text += "\n"
 
         return usage_text
-
-
-if __name__ == '__main__':
-    print(NehushtanArgumentParser().add_option('A', 'argument A', 'a').parse().get_parsed_option_dict())
--- tests/unit/cli/TestForArgumentParser.py
+++ tests/unit/cli/TestForArgumentParser.py
@@ -6,14 +6,21 @@ from nehushtan.cli.NehushtanArgumentParser import NehushtanArgumentParser
 class TestForArgumentParser(unittest.TestCase):
 
     def test_for_parse(self):
-        parser = NehushtanArgumentParser(['-a', 'aa', '--dd', 'ee', '--gg-hh', 'ii', 'j', 'k']) \
-            .add_option('A', 'arg A', short='a') \
-            .add_option('D', 'arg D', long='dd') \
-            .add_option('G', 'arg G', prefix='gg') \
+        parser = NehushtanArgumentParser(['-a', 'aa', '--dd', 'ee', '--gg-hh', 'ii', '-i', '--jj', '--k-ll']) \
+            .add_option('A', 'Option A', short='a') \
+            .add_option('D', 'Option D', long='dd') \
+            .add_option('G', 'Option G', prefix='gg') \
+            .add_option('I', 'Flag I', short='i', is_flag=True) \
+            .add_option('J', 'Flag J', long='jj', is_flag=True) \
+            .add_option('K', 'Flag K', prefix='k', is_flag=True) \
+            .add_option('L', 'Flag L', short='l', is_flag=True) \
             .parse()
 
+        print(parser.get_usage_text())
+        print(parser.get_raw_option_dict())
+
         parsed = parser.get_parsed_option_dict()
-        expected = {'A': 'aa', 'D': 'ee', 'G': {'hh': 'ii'}}
+        expected = {'A': 'aa', 'D': 'ee', 'G': {'hh': 'ii'}, 'I': True, 'J': True, 'K': {"ll": True}}
         self.assertEqual(expected, parsed)
 
     def test_for_later_parse(self):
